5bf05872b0d2c638df8277b2fa9b8743b82212c5
Daniella Rocha <darochap@uamv.edu.ni>
2025-06-30 20:41:10 -0600

    Control de dieta nutricional

---
+++ Control de Dieta Nutricional/dao/autenticacion.py
@@ -0,0 +1,13 @@
+
+CLAVE = "Admin123"  # Contraseña fija para acceso
+
+def inicio_sesion():
+    print("=== BIENVENIDO A TU CONTROL DE DIETA NUTRICIONAL ===")
+    print("=== Favor inicia sesion en tu cuenta ===")
+    intento = input("Ingresa la contraseña: ")
+    if intento == CLAVE:
+        print("Acceso permitido\n")
+        return True
+    else:
+        print("Contraseña incorrecta. Acceso denegado.\n")
+        return False
